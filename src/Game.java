9b2731f2334c9ffe52165d9ba15ae01463187c1a
tankami <95196256+tankami@users.noreply.github.com>
2022-10-14 13:22:35 -0400

    Add files via upload

---
+++ src/Game.java
@@ -0,0 +1,4 @@
+package blackjackgame;
+public static void main(String[] args) {
+    
+}
---
+++ src/Player.java
@@ -0,0 +1,24 @@
+package blackjackgame;
+import java.util.Random;
+public static String Player(playerCard) inherits Game{
+    int playerValue = 0;
+    Random cardSelect = new Random
+    int maxCardValue = 12;
+    int firstCard = cardSelect.nextInt(maxCardValue);
+    char aceCard = "A";
+    int nextCard = cardSelect.nextInt(maxCardValue);
+    String playerCard = ""
+    switch (firstCard) {
+        case firstCard == 0:
+            firstCard = 1;
+            playerCard = playerCard + "1";
+            break;
+        case firstCard == 11
+        playerCard +=  "A"
+        case
+        default:
+            playerCard = firstCard + "" + secondCard;
+            break;
+    }
+    return playerCard;
+} 
